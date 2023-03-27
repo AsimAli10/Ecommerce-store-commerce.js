b8251781d96f50119aa4f4992944f6022584ed46
Asim Ali <i190662@nu.edu.pk>
2023-03-27 12:11:01 +0500

    Get products from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Products from './components/Products/Products';
+import Navbar from './components/Navbar/Navbar';
+// import {commerce } from './lib/commerce';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => {
+        setProducts(json);
+        //console.log(json);
+      })
+      .catch(error => console.error(error));
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
   return (
     <div>
-      Ecommerce
+      <Navbar/>
+      <Products products={products}/>
     </div>
   );
 }
